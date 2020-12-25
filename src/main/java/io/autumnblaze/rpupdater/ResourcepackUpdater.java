4bf32219decf16e85c81610c2b621b7a0a418582
Alexander Liu <liujalexander84@gmail.com>
2020-12-24 20:00:10 -0800

    it builds i guess?

---
+++ src/main/java/io/autumnblaze/rpupdater/ResourcepackUpdater.java
@@ -0,0 +1,9 @@
+package io.autumnblaze.rpupdater;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ResourcepackUpdater implements ClientModInitializer {
+   @Override public void onInitializeClient() {
+      System.out.println("wofjieahoifawheoiefhopoiwfemowhaeponwaconewcnwaceonpweosefwjmiwoiegnoweigmonwefoinm");
+   }
+}
